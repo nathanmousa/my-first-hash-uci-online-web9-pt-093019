dd66c4899f5bccab6a2cff42ffc354162681efec
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-17 23:44:48 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,13 +26,9 @@ def adding
   "porcelain vases" => 2, 
   "oil paintings" => 3
   }
-  # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest["oil paintings"] = 
-  shipping_manifest["oil paintings"]
+
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
 
   shipping_manifest
-
-
-  # return the shipping_manifest hash below
-
 end
